b7b40eadc95c60b381be395149311f997af8d163
Aviv B.D. <bd.aviv@gmail.com>
2024-06-10 00:08:05 +0300

    WIP nvim: improve configuration
    Signed-off-by: Aviv B.D. <bd.aviv@gmail.com>

--- nvim/init.vim
+++ nvim/init.vim
@@ -2,3 +2,5 @@ set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 
 source ~/.vimrc
+lua require('plug_config')
+colorscheme default
---
+++ nvim/lua/plug_config.lua
@@ -0,0 +1,124 @@
+local utils = require("utils")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugin_specs = {
+  -- auto-completion engine
+  {
+    "hrsh7th/nvim-cmp",
+    -- event = 'InsertEnter',
+    event = "VeryLazy",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "onsails/lspkind-nvim",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-omni",
+      "hrsh7th/cmp-emoji",
+      "quangnguyen30192/cmp-nvim-ultisnips",
+    },
+    --config = function()
+    --  require("config.nvim-cmp")
+    --end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufRead", "BufNewFile" },
+    --config = function()
+    --  require("config.lsp")
+    --end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    enabled = function()
+      if vim.g.is_mac then
+        return true
+      end
+      return false
+    end,
+    event = "VeryLazy",
+    build = ":TSUpdate",
+    --config = function()
+    --  require("config.treesitter")
+    --end,
+  },
+
+  -- Python indent (follows the PEP8 style)
+  { "Vimjas/vim-python-pep8-indent", ft = { "python" } },
+
+  -- Python-related text object
+  { "jeetsukumaran/vim-pythonsense", ft = { "python" } },
+
+  { "machakann/vim-swap", event = "VeryLazy" },
+
+  -- AVIV
+  {
+    "dhananjaylatkar/cscope_maps.nvim",
+    dependencies = {
+      "folke/which-key.nvim", -- optional [for whichkey hints]
+      "nvim-telescope/telescope.nvim", -- optional [for picker="telescope"]
+      -- "ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
+      "nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope or fzf]
+    },
+    opts = {
+      -- USE EMPTY FOR DEFAULT OPTIONS
+      -- DEFAULTS ARE LISTED BELOW
+    },
+  },
+
+  -- Since tmux is only available on Linux and Mac, we only enable these plugins
+  -- for Linux and Mac
+  -- .tmux.conf syntax highlighting and setting check
+  {
+    "tmux-plugins/vim-tmux",
+    enabled = function()
+      if utils.executable("tmux") then
+        return true
+      end
+      return false
+    end,
+    ft = { "tmux" },
+  },
+  { 'alexghergh/nvim-tmux-navigation', config = function()
+
+    local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+    nvim_tmux_nav.setup {
+      disable_when_zoomed = true -- defaults to false
+    }
+
+    vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+    vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+    vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+    vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+    vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+    vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+  end
+}
+
+}
+
+-- configuration for lazy itself.
+local lazy_opts = {
+  ui = {
+    border = "rounded",
+    title = "Plugin Manager",
+    title_pos = "center",
+  },
+}
+
+require("lazy").setup(plugin_specs, lazy_opts)
+-- ts=2
